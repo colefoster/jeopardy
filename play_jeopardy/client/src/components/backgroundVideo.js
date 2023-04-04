f949d77aae4f8d2c3610d11be396564fb43e3b71
Cole Foster <cole@thefostersonline.com>
2023-04-03 21:40:51 -0400

    hopefully fixed bg for iphones

--- play_jeopardy/client/src/components/backgroundVideo.js
+++ play_jeopardy/client/src/components/backgroundVideo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import intro from "./video/background_intro.webm";
-
+import introMP4 from "./video/background_intro.mp4";
 
 
 function BackgroundVideo() {
@@ -9,6 +9,20 @@ function BackgroundVideo() {
     myVideo.currentTime = 7.5;
     myVideo.play();
   };
+
+  function iOS() {
+    return [
+      'iPad Simulator',
+      'iPhone Simulator',
+      'iPod Simulator',
+      'iPad',
+      'iPhone',
+      'iPod'
+    ].includes(navigator.platform)
+    // iPad on iOS 13 detection
+    || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+  } 
+
 
   return (
     <video id="bg-video"
@@ -24,7 +38,7 @@ function BackgroundVideo() {
         zIndex: "-1",
       }}
     >
-      <source src={intro} type="video/mp4" />
+      <source src={iOS() ? introMP4 : intro} type="video/mp4" />
     </video>
   );
 }
