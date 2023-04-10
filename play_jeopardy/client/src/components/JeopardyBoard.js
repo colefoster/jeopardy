31c1e06b18c22fdf61c1ccdc13b484c85ae1a158
Cole Foster <cole@thefostersonline.com>
2023-04-09 20:13:30 -0400

    began implementation of redux store

--- play_jeopardy/client/src/components/JeopardyBoard.js
+++ play_jeopardy/client/src/components/JeopardyBoard.js
@@ -1,9 +1,76 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { updateScore, revealQuestion, answerQuestion, getGameInfo } from '../redux/gameSlice'
+
+import * as Spaces from 'react-spaces'
 import '../styles/JeopardyBoard.css'
-export default class JeopardyBoard extends Component {
-  render() {
-    return (
-      <h1 >JeopardyBoard</h1>
-    )
-  }
+const JeopardyBoard = () => {
+
+  const dispatch = useDispatch();
+  const game = useSelector((state) => state.game);
+  const categories = game.categories;
+  const questions = game.questions;
+
+
+
+
+   return (
+    <>
+    <Spaces.ViewPort>
+      <Spaces.Top size="15%" style={{
+          backgroundColor: "gray",
+          opacity: 0.5,
+        }}></Spaces.Top>
+      
+        <Spaces.Left size="15%"
+        style={{
+          backgroundColor: "gray",
+          opacity: 0.5,
+        }}></Spaces.Left>
+
+        <Spaces.Right size="15%"
+        style={{
+          backgroundColor: "gray",
+          opacity: 0.5,
+        }}></Spaces.Right>
+
+        <Spaces.Bottom size="15%"
+        style={{
+          backgroundColor: "gray",
+          opacity: 0.5,
+        }}></Spaces.Bottom>
+
+        <Spaces.Fill style={{
+          backgroundColor: "blue",
+        }}>
+
+          {categories.map((category, index) => (
+            <Spaces.Left size="16.7%" style={{
+              backgroundColor: "#" + Math.floor(Math.random()*16777215).toString(16)
+            }}>
+            <div className="category">
+              <h2>{category.title}</h2>
+            </div>
+            </Spaces.Left>
+          ))}
+              
+
+
+
+        </Spaces.Fill>
+        
+      
+      
+
+        
+
+
+
+      
+      
+      
+    </Spaces.ViewPort>
+    </>
+  );
 }
+export default JeopardyBoard;
