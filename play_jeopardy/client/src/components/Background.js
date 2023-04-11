abdf25452c8ac613dffc902609066fc59a2ac72c
Cole Foster <cole@thefostersonline.com>
2023-04-11 11:45:04 -0400

    Added background toggle button

--- play_jeopardy/client/src/components/Background.js
+++ play_jeopardy/client/src/components/Background.js
@@ -1,135 +1,142 @@
-import { useCallback } from "react";
-import Particles from "react-particles";
-import { loadFull } from "tsparticles";
+import React, { useState } from 'react';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
 
-const Background = (props) => {
-    const particlesInit = useCallback(async engine => {
-        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
-        await loadFull(engine);
-    }, []);
+const Background = () => {
+  const [mode, setMode] = useState('particles');
 
-    const particlesLoaded = useCallback(async container => {
+  const particlesInit = async (engine) => {
+    await loadFull(engine);
+  };
 
-    }, []);
+  const particlesLoaded = async (container) => {};
 
-    
-    if(!props.enabled) return null
-    else if(props.mode === 'picture'){ 
-        const backgroundStyle = {
-            backgroundImage: `url("https://parade.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_1240/MTkwNTgxMTE3NzUxOTkzNDY5/jeopardy-board.webp")`,
-            backgroundSize: '100% 100%',
-            backgroundPosition: 'center center',
-            backgroundRepeat: 'no-repeat',
-            width: '100%',
-            height: '100%',
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            zIndex: 0,
-          };
-        return (
-             <div style={backgroundStyle}></div>
+  const handleToggle = () => {
+    setMode((prevMode) => (prevMode === 'particles' ? 'picture' : 'particles'));
+  };
 
-            );
-        } else {
-    return (
-         <Particles
-            id="tsparticles"
-            init={particlesInit}
-            loaded={particlesLoaded}
-            options={{
-                background: {
-                    color: {
-                        value: "#0d47a1",
-                    },
-                },
-                fpsLimit: 120,
-                interactivity: {
-                    events: {
-                        onClick: {
-                            enable: true,
-                            mode: "push",
-                        },
-                        onHover: {
-                            enable: true,
-                            mode: "connect",
-                            
-                        },
-                        resize: true,
-                    },
-                    modes: {
-                        push: {
-                            quantity: 1,
-                        },
-                        connect: {
-                            distance: 200, //how far each particle will search for other particles
-                            radius:200,//radius around mouse to affect
-                            links: {
-                                opacity: 0.5,
-                            },
-                          },
+  const particleOptions = {
+    background: {
+      color: {
+        value: '#0d47a1',
+      },
+    },
+    fpsLimit: 120,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: 'push',
+        },
+        onHover: {
+          enable: true,
+          mode: 'connect',
+        },
+        resize: true,
+      },
+      modes: {
+        push: {
+          quantity: 1,
+        },
+        connect: {
+          distance: 200,
+          radius: 200,
+          links: {
+            opacity: 0.5,
+          },
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: ['#ffffff', '#060CE9', '#8D2AB5', '#115FF4'],
+      },
+      links: {
+        color: '#ffffff',
+        distance: 150,
+        enable: false,
+        opacity: 0.5,
+        width: 1,
+      },
+      collisions: {
+        enable: false,
+        mode: 'bounce',
+      },
+      move: {
+        direction: 'none',
+        enable: true,
+        outModes: {
+          default: 'out',
+        },
+        random: false,
+        speed: 3,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800,
+        },
+        value: 80,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        options: {
+          polygon: {
+            nb_sides: 4,
+          },
+        },
+        type: 'polygon',
+      },
+      size: {
+        value: { min: 20, max: 35 },
+      },
+    },
+    detectRetina: true,
+  };
 
-                        repulse: {
-                            distance: 200,
-                            duration: 0.4,
-                        },
-                    },
-                },
-                particles: {
-                    color: {
-                        value: ["#ffffff","#060CE9", "#8D2AB5", "#115FF4"]
-                    },
-                    links: {
-                        color: "#ffffff",
-                        distance: 150,
-                        enable: false,
-                        opacity: 0.5,
-                        width: 1,
-                    },
-                    collisions: {
-                        enable: false,
-                        mode:"bounce"
-                    },
-                    move: {
-                        direction: "none",
-                        enable: true,
-                        outModes: {
-                            default: "out",
-                        },
-                        random: false,
-                        speed: 3,
-                        straight: false,
-                    },
-                    number: {
-                        density: {
-                            enable: true,
-                            area: 800,
-                        },
-                        value: 80,
-                    },
-                    opacity: {
-                        value: 0.5,
-                    },
-                    shape: {
-                      options: {
-                        polygon: {
-                          nb_sides: 4,
-                        },
-                        
-                      },
+  const imageStyle = {
+    backgroundImage: `url("https://parade.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_1240/MTkwNTgxMTE3NzUxOTkzNDY5/jeopardy-board.webp")`,
+    backgroundSize: '100% 100%',
+    backgroundPosition: 'center center',
+    backgroundRepeat: 'no-repeat',
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: 0,
+  };
 
-                        type: "polygon",
-                    },
-                    size: {
-                        value: { min: 20, max: 35 },
-                    },
-                },
-                detectRetina: true,
-            }}
-        />
-    );}
+  return (
+    <>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={particleOptions}
+        style={{ display: mode === 'particles' ? 'block' : 'none' }}
+      />
+
+      <div style={{ display: mode === 'picture' ? 'block' : 'none' }}>
+        <div style={imageStyle}></div>
+      </div>
+
+      <button onClick={handleToggle} style={{
+        flex: 1,
+        top: 50,
+        left: 50,
+        position:'relative',
+        zIndex:1,
+      }}>Toggle Background</button>
+    </>
+  );
 };
 
 export default Background;
