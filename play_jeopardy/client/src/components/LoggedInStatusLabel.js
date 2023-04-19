6d4c33912bdd6d828114263d31fbb6437ddc3974
Cole Foster <cole@thefostersonline.com>
2023-04-19 19:38:11 -0400

    Fixed logged in label

--- play_jeopardy/client/src/components/LoggedInStatusLabel.js
+++ play_jeopardy/client/src/components/LoggedInStatusLabel.js
@@ -5,11 +5,22 @@ const LoggedInStatusLabel = () => {
 
   const currentUser = useSelector(state => state.settings.user);
   console.log(currentUser);
+  let username;
+  let email;
+  try{
+    username = currentUser.username;
+    email = currentUser.email;
+  }
+  catch{
+    username= "Guest";
+    email = "";
+  }
   
   return (
     <div>
-      <h2>Logged In Status Label:</h2>
-      <h1>{currentUser}</h1>
+      <h3>Logged In Status:</h3>
+      <h2>{username}</h2>
+      <h2>{email}</h2>
 
     </div>
 
