98b7b2c9a17bb2574b16635d429f9209c5f15b05
Cole Foster <cole@thefostersonline.com>
2023-04-23 22:51:08 -0400

    change to allow all cors origins

--- play_jeopardy/server/server.js
+++ play_jeopardy/server/server.js
@@ -86,7 +86,7 @@ app.use(cors({
     if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true)
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(null, true)
     }
   },
   credentials: true
